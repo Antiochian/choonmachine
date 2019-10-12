a0cf38472693794ef077f4d7d43e4e7ce890975a
Antioch <53316182+Antiochian@users.noreply.github.com>
2019-10-13 00:24:27 +0100

    Update liteCHOON.sh

--- liteCHOON.sh
+++ liteCHOON.sh
@@ -18,7 +18,7 @@ then #ONLINE MODE
 	do
 		omxplayer --vol -900 -o local -b /home/pi/loadedchoons/current.mov & #plau current file
 		#Download next file WHILE video is playing to save time
-		sshpass -f '/home/pi/SSHpassword.txt' scp -o PreferredAuthentications=password -v trin3161@linux.ox.ac.uk:$f /home/pi/loadedchoons/next.mov &
+		sshpass -f '/home/pi/SSHpassword.txt' scp -o PreferredAuthentications=password -v XXXX@XXXX:$f /home/pi/loadedchoons/next.mov &
 		wait #wait for both video and download to finish
 		#Change "next" file to "current" for loop
 		cp /home/pi/loadedchoons/next.mov /home/pi/loadedchoons/current.mov
