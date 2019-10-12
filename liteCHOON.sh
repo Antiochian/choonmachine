6475ddd8811f2c358bebccfd5bcf5a96edbc18a8
Antioch <53316182+Antiochian@users.noreply.github.com>
2019-10-12 23:57:26 +0100

    Update liteCHOON.sh
    Added parallel execution of commands to remove wait times

--- liteCHOON.sh
+++ liteCHOON.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
-
-while : #loop forever
-do
-
 #First, check if the last run has left a "current.mov" file to play
 if test ! -f "/home/pi/loadedchoons/current.mov"
-then
+then #if no file is found, import local "startup" video to begin with
 	echo "Initial file not properly loaded, Importing locally..."
 	cp loadedchoons/startup.mov loadedchoons/current.mov
 else
@@ -13,23 +9,15 @@ else
 fi
 
 #MAIN LOOP
-for f in $(sshpass -f 'SSHpassword.txt' ssh trin3161@linux.ox.ac.uk ls WIP2/*.mov | shuf) #randomly reorders contents of webserver folder
+#randomly reorders contents of webserver folder and iterates through filenames
+for f in $(sshpass -f 'SSHpassword.txt' ssh trin3161@linux.ox.ac.uk ls IDENTS/*.mov | shuf) 
 do
-echo "Filename: $f"
-#Play current file
-omxplayer --vol -900 -o local -b /home/pi/loadedchoons/current.mov
-#Download next file
-sshpass -f '/home/pi/SSHpassword.txt' scp trin3161@linux.ox.ac.uk:$f /home/pi/loadedchoons/current.mov
-done;
-exit 0
-for f in $(sshpass -f 'SSHpassword.txt' ssh trin3161@linux.ox.ac.uk ls IDENTS/*.mov | shuf) #randomly reorders contents of webserver folder
-do
-	#Download next file
-	sshpass -f '/home/pi/SSHpassword.txt' scp -o PreferredAuthentications=password -v trin3161@linux.ox.ac.uk:$f /home/pi/loadedchoons/next.mov
-	omxplayer --vol -900 -o local -b /home/pi/loadedchoons/current.mov #plau current file
+	omxplayer --vol -900 -o local -b /home/pi/loadedchoons/current.mov & #plau current file
+	#Download next file WHILE video is playing to save time
+	sshpass -f '/home/pi/SSHpassword.txt' scp -o PreferredAuthentications=password -v trin3161@linux.ox.ac.uk:$f /home/pi/loadedchoons/next.mov &
+	wait #wait for both video and download to finish
 	#Change "next" file to "current" for loop
 	cp /home/pi/loadedchoons/next.mov /home/pi/loadedchoons/current.mov
 	echo "Up Next: $f" #print current file
-done
 done
 exit 0
