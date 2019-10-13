673fcf91e0398982dfab619deb7e2fa5acf965ab
Antioch <53316182+Antiochian@users.noreply.github.com>
2019-10-13 05:22:36 +0100

    Update optCHOON.sh

--- optCHOON.sh
+++ optCHOON.sh
@@ -1,30 +1,31 @@
 #! /bin/bash
 # This version of the program accepts a RegEx string as an argument for tagfiltering
 
-if [ ! $# == ]
+if [ ! $# == 0 ] #if an argument is supplied
 then
   FILTER=$1
   echo "Custom filter applied: $1"
 else
   FILTER='*.*'
 fi
-
+rm "/home/pi/loadedchoons/current.mov"
 #CHECK INTERNET CONNECTION
 if ping -1 -c 1 -W 2 google.com
 then #ONLINE MODE
 	echo "Network connection successful"
-	#First, check if the last run has left a "current.mov" file to play
-	if test ! -f "/home/pi/loadedchoons/current.mov"
-	then #if no file is found, import local "startup" video to initialise
-		echo "Initial file not properly loaded, Importing locally..."
-		cp loadedchoons/startup.mov loadedchoons/current.mov
-	else
-		echo "Initial file loaded successfully"
-	fi
-	
 	#randomly reorders contents of webserver folder and iterates through filenames
 	for f in $(sshpass -f 'SSHpassword.txt' ssh trin3161@linux.ox.ac.uk ls CURRENT/*.mov | grep -E $FILTER | shuf) 
 	do
+		if test ! -f "/home/pi/loadedchoons/current.mov" #only runs on first play
+		then
+			echo "Up Next: $f"
+			sshpass -f '/home/pi/SSHpassword.txt' scp -o PreferredAuthentications=password -v XXXX@XXXX:$f /home/pi/loadedchoons/current.mov
+			omxplayer --vol -900 -o local -b /home/pi/loadedchoons/current.mov #play current file
+			wait #wait for download to finish if it hasnt already
+			#Change "next" file to "current" for loop
+			cp /home/pi/loadedchoons/next.mov /home/pi/loadedchoons/current.mov
+			echo "Up Next: $f" #print current file
+		else
 		#Download next file in background WHILE video is playing to save time
 		sshpass -f '/home/pi/SSHpassword.txt' scp -o PreferredAuthentications=password -v XXXX@XXXX:$f /home/pi/loadedchoons/next.mov &
 		omxplayer --vol -900 -o local -b /home/pi/loadedchoons/current.mov #play current file
@@ -32,6 +33,7 @@ then #ONLINE MODE
 		#Change "next" file to "current" for loop
 		cp /home/pi/loadedchoons/next.mov /home/pi/loadedchoons/current.mov
 		echo "Up Next: $f" #print current file
+		fi
 	done
 else #OFFLINE MODE
 	echo "Failed connection to network, reverting to offline storage..."
